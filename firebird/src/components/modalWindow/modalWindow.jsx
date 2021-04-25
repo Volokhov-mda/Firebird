4c4f9ce2b3f39e0ab9358a13782ab6b65685a3bc
Никита Волохов <navolokhov@edu.hse.ru>
2021-04-25 03:11:08 +0300

    Fixed modal window height for mobile devices browsers such as Safari or Google Chrome

--- firebird/src/components/modalWindow/modalWindow.jsx
+++ firebird/src/components/modalWindow/modalWindow.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./modalWindow.scss";
 
 export default function ModalWindow({active, setActive, children, asset}) {
+    const windowHeight = window.innerHeight;
+
     return (
-        <div className="modal-window" onClick={() => setActive(false)}>
+        <div className="modal-window" onClick={() => setActive(false)} style={{height: windowHeight + "px"}}>
             <div className="modal-window-content" onClick={e => e.stopPropagation()} aria-label={`Всплывающее окно ассета ${asset}`}>
                 {children}
             </div>
